65504f76a8f6aa610d243bc70cb4923fb66b8d57
Jonas Hansen <jonas.hansen@visma.com>
2022-01-26 15:42:04 +0100

    Add project files.

---
+++ H3-MilkAndCookies/Controllers/MilkshakeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkAndCookies.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MilkshakeController : ControllerBase
+    {
+        [HttpGet(Name = "GetMilkshake")]
+        public string Get(string favoriteMilkshake)
+        {
+            return "Your favorite milkshake is " + favoriteMilkshake;
+        }
+    }
+}
